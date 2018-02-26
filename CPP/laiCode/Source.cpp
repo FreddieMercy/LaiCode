77884f1a82282d5a187b464662c6d41ae8cc6758
FreddieMercy <junhao.zhang.freddie@gmail.com>
2018-02-26 14:49:27 -0800

    CPP renamed/removed

---
+++ CPP/laiCode/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <queue>
+#include <fstream>
+#include <string>
+#include <bitset>
+#include <sstream>
+#include <climits>
+#include <cmath>
+#include <map>
+#include <utility>
+#include <stdint.h>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include "_IsBinarySearchTreeOrNot.h"
+using namespace std;
+
+int main(int argc, char*argv[])
+{
+	cout << "Pass" << endl;
+}
---
+++ CPP/laiCode/_IsBinarySearchTreeOrNot_.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <queue>
+#include <fstream>
+#include <string>
+#include <bitset>
+#include <sstream>
+#include <climits>
+#include <cmath>
+#include <map>
+#include <utility>
+#include <stdint.h>
+#include <algorithm>
+#include <stack>
+#include <set>
+
+using namespace std;
+
+
+class TreeNode {
+public:
+	int value;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int v) : value(v), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	bool isBST(TreeNode* root, int Limit = INT_MAX, int Parent = INT_MAX, bool isLeft = true, bool isTwist = false) {
+		return root == NULL || (((isLeft^isTwist) ? (root->value < Limit) : (root->value > Limit)) && (root->left == NULL || root->left->value<root->value) && (root->right == NULL || root->right->value> root->value) && isBST(root->left, ((!isLeft) ? Parent : root->value), root->value, true, !isLeft) && isBST(root->right, ((isLeft) ? Parent : root->value), root->value, false, isLeft));
+	}
+};
